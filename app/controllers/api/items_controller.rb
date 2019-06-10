2eb5e087fecbe7a201a94eed433f5ffd8d131094
Amanda Garcia <algarcian@outlook.com>
2019-06-10 14:42:10 -0600

    Items controller added

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,3 +1,45 @@
 class Api::ItemsController < ApplicationController
-  
+  before_action :set_menu,
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+
+  def index
+    render json: @menu.items.all
+  end 
+
+  def show 
+  end 
+
+  def create
+    item = @menu.items.new(item_params)
+    if item.save 
+      render json: item 
+    else 
+      render json: {errors: item.errors}, status: :unprocessable_entity
+    end 
+  end 
+
+  def edit 
+  end 
+
+  def update
+  end 
+
+  def destroy
+    @item.destroy 
+    render json: {message: "Item Deleted"}
+ 
+  end 
+
+  private 
+  def set_menu 
+    @menu = Menu.find(params[:menu_id])
+  end 
+
+  def set_item
+    @item = Item.find(params[:id])
+  end 
+
+  def item_params
+    params.require(:item).permit(:name, :price)
+  end 
 end
